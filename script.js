9630852836e5e36955d72483c696c8680a609102
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2026-02-27 09:36:00 -0700

    added button

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const here = document.getElementById("puzzel");
 const control = document.getElementById("set");
+const count = document.getElementById("count");
+const range = document.getElementById("input");
+const submitBTN =document.getElementById("submit");
+const setSEC = document.getElementById("set");
+const puzzelSEC = document.getElementById("puzzel");
+
+
+range.addEventListener("change", () => {
+    count.textContent = range.value;
+    console.log(range.value);
+});
+
+submitBTN.addEventListener("click", () => {
+    setSEC.classList.toggle("hide");
+    puzzelSEC.classList.toggle("hide");
+});
+
 
 function populate() {
     for(let i=0; i<LIST; i++) {
